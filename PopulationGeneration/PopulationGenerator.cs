3592f04acaf474418259a9e7dd7cbad64666f54c
Elchanan Razin <etrazin@gmail.com>
2019-12-19 17:05:10 +0200

    population generation commit

--- PopulationGeneration/PopulationGenerator.cs
+++ PopulationGeneration/PopulationGenerator.cs
@@ -1,19 +1,29 @@
-﻿using System;
+﻿using Crosscutting;
+using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace PopulationGeneration
 {
     public class PopulationGenerator
     {
-        public List<string> Generate(int populationSize,int boardSize)
+        private readonly Random _random = new Random();
+
+        public List<string> Generate(int populationSize, int boardSize)
         {
             List<string> population = new List<string>();
             for (int i = 0; i < populationSize; i++)
             {
-                
+                StringBuilder chromosome = new StringBuilder();
+                //generate strings of length boardSize*2 so that they'll  be equal to the shortest possible path from start to end
+                for (int j = 0; j < boardSize * 2; j++)
+                {
+                    //append U R D or L to the string/chromosome
+                    chromosome.Append(Enum.GetName(typeof(Movements), _random.Next(0, 4)));
+                }
+                population.Add(chromosome.ToString());
             }
             return population;
         }
-
     }
 }
