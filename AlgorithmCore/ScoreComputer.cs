85cc9508b5082080ca8b8293f67de003d3c05c70
Elchanan Razin <etrazin@gmail.com>
2019-12-19 19:33:52 +0200

    added score computer

---
+++ AlgorithmCore/ScoreComputer.cs
@@ -0,0 +1,14 @@
+﻿using Crosscutting;
+using System;
+using System.Collections.Generic;
+
+namespace AlgorithmCore
+{
+    public class ScoreComputer
+    {
+        public List<Chromosome> ComputeScores(List<Chromosome> population,int boardSize)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
