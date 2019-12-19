344d55f86755f6c5511a9acd828b931769f707ea
Elchanan Razin <etrazin@gmail.com>
2019-12-19 17:25:42 +0200

    updated population generator to work with Chromosome object

--- PopulationGeneration/PopulationGenerator.cs
+++ PopulationGeneration/PopulationGenerator.cs
@@ -9,19 +9,22 @@ namespace PopulationGeneration
     {
         private readonly Random _random = new Random();
 
-        public List<string> Generate(int populationSize, int boardSize)
+        public List<Chromosome> Generate(int populationSize, int boardSize)
         {
-            List<string> population = new List<string>();
+            List<Chromosome> population = new List<Chromosome>();
             for (int i = 0; i < populationSize; i++)
             {
-                StringBuilder chromosome = new StringBuilder();
+                StringBuilder pathString = new StringBuilder();
                 //generate strings of length boardSize*2 so that they'll  be equal to the shortest possible path from start to end
                 for (int j = 0; j < boardSize * 2; j++)
                 {
                     //append U R D or L to the string/chromosome
-                    chromosome.Append(Enum.GetName(typeof(Movements), _random.Next(0, 4)));
+                    pathString.Append(Enum.GetName(typeof(Movements), _random.Next(0, 4)));
                 }
-                population.Add(chromosome.ToString());
+                Chromosome chromosome = new Chromosome();
+                chromosome.PathString = pathString.ToString();
+
+                population.Add(chromosome);
             }
             return population;
         }
