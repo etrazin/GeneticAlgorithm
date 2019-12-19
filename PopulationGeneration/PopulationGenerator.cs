e6052950d4b4bdafa1a1f132e52b52fec919577a
Elchanan Razin <etrazin@gmail.com>
2019-12-19 15:08:16 +0200

    recommit crosscutting with solution file

---
+++ PopulationGeneration/PopulationGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PopulationGeneration
+{
+    public class PopulationGenerator
+    {
+        public List<string> Generate(int populationSize,int boardSize)
+        {
+            List<string> population = new List<string>();
+            for (int i = 0; i < populationSize; i++)
+            {
+                
+            }
+            return population;
+        }
+
+    }
+}
