1ee93721d68e7c0a237721a9a54ef60f07e1b756
Elchanan Razin <etrazin@gmail.com>
2019-12-19 19:34:15 +0200

    added fitness selector (selects parents by fitness)

---
+++ AlgorithmCore/FitnessSelector.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Crosscutting;
+using System.Collections.Generic;
+namespace AlgorithmCore
+{
+    public class FitnessSelector
+    {
+        public List<Chromosome> Select(List<Chromosome> population)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
