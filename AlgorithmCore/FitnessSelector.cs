2f8ea1ac516a3c82bd4407bf515af53d6dd64a03
Elchanan Razin <etrazin@gmail.com>
2019-12-23 18:36:29 +0200

    implemented roulette wheel fitness selection

--- AlgorithmCore/FitnessSelector.cs
+++ AlgorithmCore/FitnessSelector.cs
@@ -1,15 +1,35 @@
 ﻿using System;
 using Crosscutting;
 using System.Collections.Generic;
+using System.Linq;
 namespace AlgorithmCore
 {
     public class FitnessSelector
     {
         //selects a pair of chromosomes from population to be parents
         //selection is done randomly with each chromosome having probability by fitness using roulette wheel selection
-        public List<Chromosome> Select(List<Chromosome> population)
+        public Chromosome Select(List<Chromosome> population)
         {
-            throw new NotImplementedException();
+            Chromosome selectedChromosome = null;
+            double fitnessValuesSum = 0;
+            //generate random value between 0 and 1
+            Random random = new Random();
+            double randomValue = random.NextDouble();
+
+            //sort population by fitness value
+            population = population.OrderBy(chrom => chrom.FitnessValue).ToList();
+            
+            //sum the fitness values and select the first that is larger than probability value
+            foreach (Chromosome chromosome in population)
+            {
+                fitnessValuesSum = fitnessValuesSum + chromosome.FitnessValue;
+                if(randomValue<fitnessValuesSum)
+                {
+                    selectedChromosome = chromosome;
+                    break;
+                }
+            }
+            return selectedChromosome;
         }
     }
 }
