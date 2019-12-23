297bcb56855edef1d4e6fcacc4ac5e8314e1cd78
Elchanan Razin <etrazin@gmail.com>
2019-12-23 15:12:51 +0200

    added main flow of algorithm

---
+++ AlgorithmCore/GeneticAlgorithm.cs
@@ -0,0 +1,36 @@
+﻿using Crosscutting;
+using PopulationGeneration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmCore
+{
+    
+    public class GeneticAlgorithm
+    {
+        //flow of genetic algorithm
+        public List<Chromosome> GenerateNextGeneration(int populationSize,int boardSize)
+        {
+            //generate population
+            List<Chromosome> nextGenerationPopulation = new List<Chromosome>();
+            List<Chromosome> population = new List<Chromosome>();
+
+            PopulationGenerator populationGenerator = new PopulationGenerator();
+            population = populationGenerator.Generate(populationSize, boardSize);
+
+            //compute scores for each member of population
+            ScoreComputer scoreComputer = new ScoreComputer();
+            population = scoreComputer.ComputeScores(population, boardSize);
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                //select
+                //crossover
+                //mutation
+                //add to next generation
+            }
+            return nextGenerationPopulation;
+        }
+    }
+}
