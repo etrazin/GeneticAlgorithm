38137692002507e038c753c4ea4045770de81247
Elchanan Razin <etrazin@gmail.com>
2019-12-23 15:12:07 +0200

    added comment

--- AlgorithmCore/FitnessSelector.cs
+++ AlgorithmCore/FitnessSelector.cs
@@ -5,6 +5,8 @@ namespace AlgorithmCore
 {
     public class FitnessSelector
     {
+        //selects a pair of chromosomes from population to be parents
+        //selection is done randomly with each chromosome having probability by fitness using roulette wheel selection
         public List<Chromosome> Select(List<Chromosome> population)
         {
             throw new NotImplementedException();
