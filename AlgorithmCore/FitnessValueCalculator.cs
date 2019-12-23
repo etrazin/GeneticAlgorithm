b42e485634fc72e43d1413c4dfda4efa8aa79c24
Elchanan Razin <etrazin@gmail.com>
2019-12-23 16:26:31 +0200

    added fitness value calculator

---
+++ AlgorithmCore/FitnessValueCalculator.cs
@@ -0,0 +1,17 @@
+﻿using Crosscutting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmCore
+{
+    //calculates fitness values for population
+    //fitness value=score/sum of scores
+    public class FitnessValueCalculator
+    {
+        public List<Chromosome> Calculate(List<Chromosome> population)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
