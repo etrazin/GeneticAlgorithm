4a08bf5bfd29695192122d91544e4cdbbd8c25db
Elchanan Razin <etrazin@gmail.com>
2020-01-02 18:34:29 +0200

    implemented algorithm main flow

--- AlgorithmCore/GeneticAlgorithm.cs
+++ AlgorithmCore/GeneticAlgorithm.cs
@@ -3,34 +3,53 @@ using PopulationGeneration;
 using System;
 using System.Collections.Generic;
 using System.Text;
-
+using System.Linq;
 namespace AlgorithmCore
 {
-    
     public class GeneticAlgorithm
     {
-        //flow of genetic algorithm
-        public List<Chromosome> GenerateNextGeneration(int populationSize,int boardSize)
+        public void RunAlgorithm(int populationSize,int boardSize)
         {
-            //generate population
-            List<Chromosome> nextGenerationPopulation = new List<Chromosome>();
+            Chromosome correctPath = null;
             List<Chromosome> population = new List<Chromosome>();
 
+            //generate initial population
             PopulationGenerator populationGenerator = new PopulationGenerator();
             population = populationGenerator.Generate(populationSize, boardSize);
 
-            //compute scores for each member of population
-            ScoreComputer scoreComputer = new ScoreComputer();
-            population = scoreComputer.ComputeScores(population, boardSize);
-
-            for (int i = 0; i < populationSize; i++)
+            NextGenerationGenerator nextGenerationGenerator = new NextGenerationGenerator();
+            
+            int generationNumber = 0;
+            while (generationNumber<10000)
             {
-                //select
-                //crossover
-                //mutation
-                //add to next generation
+                //compute scores for each member of population
+                ScoreComputer scoreComputer = new ScoreComputer();
+                population = scoreComputer.ComputeScores(population, boardSize);
+
+                //if correct path is found then print it
+                correctPath = population.FirstOrDefault(chrom => chrom.Score == boardSize * 2);
+                if (correctPath!=null)
+                {
+                    Console.WriteLine("correct path:" + correctPath.PathString);
+                }
+                
+                //calculate fitness of each chromosome
+                FitnessValueCalculator fitnessValueCalculator = new FitnessValueCalculator();
+                population = fitnessValueCalculator.Calculate(population);
+
+                population = nextGenerationGenerator.GenerateNextGeneration(population, populationSize, boardSize);
+                //foreach (var item in population)
+                //{
+                //    Console.WriteLine(item.PathString);
+                //}
+                //Console.WriteLine(population.Select(chrom => chrom.Score).Max());
+                //Console.WriteLine("generation number" + generationNumber);
+                generationNumber++;
             }
-            return nextGenerationPopulation;
+            if(generationNumber==9999)
+            {
+                Console.WriteLine("correct path not found");
+            }
         }
     }
 }
